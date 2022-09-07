d500def1a573b2d73a69810a4993a1481e42077c
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-07 19:50:41 +0300

    Typescript

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+let number = 1;
+let my_name = "John";
+function sucker(document) {
+    console.log(document);
+}
+let my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let empty_array = [];
+empty_array[0] = true;
+my_array.forEach((element) => {
+    element.toPrecision(2);
+});
+let user = ["John", 1];
+user[1].toString();
+user.push(1);
+console.log("Blue");
+//# sourceMappingURL=index.js.map
